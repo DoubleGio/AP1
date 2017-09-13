ebbba97ad9c392557aef37a2e1a543feb14fc715
DoubleGio <31928779+DoubleGio@users.noreply.github.com>
2017-09-13 16:38:12 +0200

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,86 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main implements CalculatorInterface {
+    
+    private static final String OPERATOR_TOKENS = "+-*/^";
 
-
+	private boolean isNumber(String token) {
+		try(Scanner in = new Scanner(token)) {
+			return in.hasNextInt();
+		}
+	}
+	
+	private boolean isOperator(String token) {
+		try(Scanner in = new Scanner(token)) {
+			try(Scanner operatorScanner = new Scanner(OPERATOR_TOKENS)) {
+				while (operatorScanner.hasNext()) {
+					if (in.equals(operatorScanner.next())) {
+						return true;
+					} 
+				}
+				return false;
+			}
+		}
+	}
+	
+	private int whichPresedence(String token) {
+		int presedence = 0;
+		switch (token) {
+			case "+": presedence = 0;
+				break;
+			case "-": presedence = 0;
+				break;
+			case "*": presedence = 1;
+				break;
+			case "/": presedence = 1;
+				break;
+			case "^": presedence = 2;
+				break;
+		}
+		return presedence;
+	}
+	
+	private boolean isParenthesis(String token) {
+		if (token.equals("(") | token.equals(")")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
     public TokenList readTokens(String input) {
-        // TODO: Implement this
-        return null;
+        TokenList result = new TokenList_Imp(input.replaceAll("\\s+","").length());
+		Scanner in = new Scanner(input);
+		while (in.hasNext()) {
+			String token = in.next();
+			int type;
+			int precedence;
+			int extra_precedence = 0;
+			if (isNumber(token)) {
+				type = 1;
+				precedence = -1 + extra_precedence;
+			} else if (isOperator(token)) {
+				type = 2;
+				precedence = whichPresedence(token) + extra_precedence;
+			} else if (isParenthesis(token)) {
+				type = 3;
+				precedence = 3;
+				if (token.equals("(")) {
+				    extra_precedence++;
+				} else if (token.equals(")")) {
+				    extra_precedence--;
+				}
+			} else {
+				PrintStream out = new PrintStream(System.out);
+				out.print("ERROR: unknown character");
+				//System.exit(1);
+				break;
+			}
+			result.add(new Token_Imp(token, type, precedence));
+		}
+		in.close();
+		return result;
     }
-
 
     public Double rpn(TokenList tokens) {
         // TODO: Implement this
