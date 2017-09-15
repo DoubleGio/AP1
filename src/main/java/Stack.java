1d06c7e6eed37f7dab932c3b24b876c2575cde2e
DoubleGio <31928779+DoubleGio@users.noreply.github.com>
2017-09-15 15:14:05 +0200

    Update Stack.java

--- src/main/java/Stack.java
+++ src/main/java/Stack.java
@@ -4,9 +4,27 @@ public class Stack implements TokenStack {
 	private Token[] tokenStack;
 	private int topOfStack;
 	
-	public Stack () {
-		tokenStack = new Token[10];
+	public Stack (TokenList tokens) {		//FIX DIT ofzo
+		tokenStack = new Token[tokens.size()];
 		topOfStack = -1;
+		while (topOfStack < tokens.size()) {
+			topOfStack++;
+        	Token token = tokens.get(topOfStack);
+        	int type = token.getType();
+        	if (type == 1) {
+        		tokenStack[topOfStack] = token;
+        	} else if (type == 2) {
+        		Token token2 = tokens.get(topOfStack + 1);
+        		if (token2.getType() == 3) {
+        			
+        		}
+        		tokenStack[topOfStack] = token2;
+        		topOfStack++;
+        		tokenStack[topOfStack] = token;
+        	} else {	//type = 3		3x(4-5)	--> 3 4 5 - x
+        		
+        	}
+        }
 	}
 
 	public void push(Token token) {
