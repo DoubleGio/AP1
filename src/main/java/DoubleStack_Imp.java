99628fd9be3ac301371f8e260742d86642a7a78e
JaroAmsterdam <31955344+JaroAmsterdam@users.noreply.github.com>
2017-09-18 17:23:16 +0200

    Create DoubleStack_Imp.java

---
+++ src/main/java/DoubleStack_Imp.java
@@ -0,0 +1,44 @@
+
+public class DoubleStack_Imp implements DoubleStack {
+	
+	private double[] doubleStack;
+	private int topOfStack;
+	
+	public DoubleStack_Imp () {
+		doubleStack = new double[10];
+		topOfStack = -1;
+	}
+	
+    public void push(Double element) {
+    	topOfStack ++;
+    	if (topOfStack == doubleStack.length) {
+    		double[] tempStack = new double[doubleStack.length * 2];
+    		System.arraycopy(doubleStack, 0, tempStack, 0, doubleStack.length - 1);
+    		tempStack[topOfStack] = element;
+    		doubleStack = tempStack;
+    	}
+    }
+
+    public Double pop() {
+    	if (topOfStack <= -1) {
+			//something error
+		}
+		topOfStack--;
+		return doubleStack[topOfStack + 1];
+    }
+
+    public Double top() {
+    	if (topOfStack <= -1) {
+    		//something error
+    	}
+    	try {
+			return doubleStack[topOfStack];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return null;
+		}
+    }
+
+    public int size() {
+    	return topOfStack + 1;
+    }
+}
